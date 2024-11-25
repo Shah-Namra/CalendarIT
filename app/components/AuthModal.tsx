f58a2da2bbd95fcbd407b236bbcf2684e28ba01b
Shah-Namra <21it106@svitvasad.ac.in>
2024-11-25 11:08:31 +0530

    Initial commit from Create Next App project setup i

---
+++ app/components/AuthModal.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '@radix-ui/react-dialog';
+import Logo from '@/public/logo.png';
+import Image from "next/image";
+import { signIn } from "../lib/auth";
+import { GitHubAuthButton, GoogleAuthButton } from "./SubmitButtons";
+
+export function AuthModal() {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button>Try For Free</Button>
+            </DialogTrigger>
+
+            <DialogContent className="sm:max-w-[360px]">
+                <DialogHeader className="flex-row items-center justify-center gap-2">
+                    <Image src={Logo} alt="Logo" className="size-10" />
+                    <h4 className="text-3xl font-semibold">
+                        Calendar
+                        <span className="text-primary">IT</span>
+                    </h4>
+                </DialogHeader>
+                <div className="flex flex-col mt-5 gap-4">
+                    <form
+                        action={async () => {
+                            "use server";
+                            await signIn("google");
+                        }}
+                        className="w-full"
+                    >
+                        <GoogleAuthButton />
+                    </form>
+
+                    <form
+                        action={async () => {
+                            "use server";
+                            await signIn("github");
+                        }}
+                        className="w-full"
+                    >
+                        <GitHubAuthButton />
+                    </form>
+                </div>
+            </DialogContent>
+        </Dialog>
+    );
+}
